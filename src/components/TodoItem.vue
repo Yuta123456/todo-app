72c8da6378fbe919d478bc4f5ebac5b33c3c3d55
Yuta123456 <yuuta09090530@icloud.com>
2022-07-06 21:26:02 +0900

    TodoアプリをCompositionAPIで作成

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    {{ getTitle }}
+    <button @click="$emit('remove')">Remove</button>
+  </li>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+export default defineComponent({
+  name: "TodoItem",
+  components: {},
+  props: {
+    title: String,
+  },
+  setup(props) {
+    const getTitle = computed(() => props.title);
+    return {
+      getTitle,
+    };
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div id="todo-list-example">
+      <form v-on:submit.prevent="addNewTodo">
+        <label for="new-todo">Add a todo</label>
+        <input
+          v-model="newTodoText"
+          id="new-todo"
+          placeholder="E.g. Feed the cat"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        <todo-item
+          v-for="(todo, index) in todos"
+          :key="todo.id"
+          :title="todo.title"
+          @remove="todos.splice(index, 1)"
+        ></todo-item>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-@Options({
+import { defineComponent, ref } from "vue";
+import TodoItem from "@/components/TodoItem.vue";
+// import { Options } from "vue-class-component";
+export default defineComponent({
+  name: "HomeView",
   components: {
-    HelloWorld,
+    TodoItem,
   },
-})
-export default class HomeView extends Vue {}
+  setup() {
+    const todos = ref([
+      {
+        id: 1,
+        title: "Do the dishes",
+      },
+      {
+        id: 2,
+        title: "Take out the trash",
+      },
+      {
+        id: 3,
+        title: "Mow the lawn",
+      },
+    ]);
+    const newTodoText = ref("");
+    let nextTodoId = 4;
+    const addNewTodo = () => {
+      console.log("add fire");
+      todos.value.push({
+        id: nextTodoId,
+        title: newTodoText.value,
+      });
+      nextTodoId++;
+      newTodoText.value = "";
+    };
+    return {
+      todos,
+      newTodoText,
+      addNewTodo,
+    };
+  },
+});
 </script>
